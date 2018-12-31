1ef9872413473a099cce1fc2ceef5e3fe08dca79
Pankaj <pankaj.gajewar@techprimelab.com>
2018-12-31 18:40:05 +0530

    like & comment operations are done

--- src/Components/Home/HomeComponent.js
+++ src/Components/Home/HomeComponent.js
@@ -236,22 +236,3 @@ class HomeComponent extends Component {
 }
 
 export default HomeComponent;
-
-{/* <div className="card-body">
-    <div className="row">
-        <div className="col-md-2">
-            <img src={logo} width="50px" height="50px" alt="Unable to load image" />
-        </div>
-        <div className="col-md-8 h5 text-left my-auto">
-            Student Name
-                                </div>
-        <p className="m-3 text-justify">
-            <b>#Goodreview #welldone</b>
-            <br></br>
-            ./src/shared/Footer.js
-              Line 10:  Using target="_blank" without rel="noopener noreferrer" is a security risk: see https://mathiasbynens.github.io/rel-noopener  react/jsx-no-target-blank
-            ./src/shared/Header.js
-              Line 11:  Redundant alt attribute. Screen-readers already announce `img` tags as an image. You don’t need to use the words `image`, `photo,` or `picture` (or any specified custom words) in the alt prop  jsx-a11y/img-redundant-alt
-                                </p>
-    </div>
-</div> */}
